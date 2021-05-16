25ebe838a8b71a86c06074635b8a5bf5f6cd083e
satthabut <satthabut.o@gmail.com>
2021-05-16 17:52:14 +0700

    - [feat] update auth in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { AppBar, Button, Typography, Toolbar ,Avatar} from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory ,useLocation} from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { AppBar, Button, Typography, Toolbar, Avatar } from "@material-ui/core";
 import useStyles from "./styles";
 import story from "../../images/memories.png";
 
 const Navbar = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
 
-  const user = null;
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+
+    history.push("/");
+    setUser(null)
+  };
+  useEffect(() => {
+    const token = user?.token;
+
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, [location]);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
@@ -40,6 +55,7 @@ const Navbar = () => {
               variant="contained"
               className={classes.logout}
               color="secondary"
+              onClick={logout}
             >
               Logout
             </Button>
